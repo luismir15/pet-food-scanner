746952985cc59490b4cfa534353d1d0ae1275ef4
Akarsh Jain <akarsh.jain.790@gmail.com>
2023-05-21 15:09:02 +0530

    ui: Added onboarding screen.
    Implemented an onboarding screen to provide a seamless user
    introduction and enhance the user experience.

---
+++ lib/helper/color_sys.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorSys {
+  static Color primary = const Color.fromRGBO(52, 43, 37, 1);
+  static Color grey = const Color.fromRGBO(137, 137, 137, 1);
+  static Color secondary = const Color.fromRGBO(144, 202, 249, 1);
+}
---
+++ lib/helper/strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  static var stepOneTitle = "Nutrition Guide";
+  static var stepOneContent =
+      "Discover the best food options for your pet's well-being and keep them healthy";
+  static var stepTwoTitle = "Product Analyzer";
+  static var stepTwoContent =
+      "Scan and evaluate pet products to ensure their safety and quality for your furry friend";
+  static var stepThreeTitle = "PetCare Essentials";
+  static var stepThreeContent =
+      "Access a comprehensive database of pet products and food, helping you make informed choices for your pet's care";
+}
